da3500fe0a285e7379e698cd50ad91153236bd69
T <devt@deloitte.com>
2025-10-06 12:39:28 +0530

    add

--- com/Features.java
+++ com/Features.java
@@ -1,22 +1,7 @@
 package com.example;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class FeatureTracker {
     public static void main(String[] args) {
-        listFeatures();
-    }
-
-    public static void listFeatures() {
-        try (BufferedReader reader = new BufferedReader(new FileReader("features.txt"))) {
-            String line;
-            System.out.println("Feature List:");
-            while ((line = reader.readLine()) != null) {
-                System.out.println("- " + line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Feature Tracker App");
     }
 }
